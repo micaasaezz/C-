0c0af6d86b6b6d685558bb74bef69954240162f3
Micaela Saez <mica_saez@hotmail.com>
2017-08-16 12:10:10 -0300

    16agosto

---
+++ claseUno/ejercicioUno/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioUno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int maximo=0;
+            int minimo=0;
+            int contador;
+            int acumulador=0;
+            float promedio;
+            int primerNumeroPar;
+            bool encontrPrimerNumeroPar=false;
+            int cantidadPares = 0;
+            int lugarDelPrimerPar;
+            int lugarDelUltimoImpar;
+
+            /*
+             while(contador<5)
+             {
+                contador++;
+             }
+            */
+            for (contador = 0; contador < 5; contador++)
+            {
+                Console.WriteLine("Ingrese el numero");
+                while(! int.TryParse(Console.ReadLine(), out numero))
+                {
+                    Console.WriteLine("Error, reingrese");
+                }
+
+                if (contador == 0)
+                {
+                    maximo = numero;
+                    minimo = numero;
+                }
+                else
+                {
+                    if (numero > maximo)
+                    {
+                        maximo = numero;
+                    }
+
+                    if (numero < minimo)
+                    {
+                        minimo = numero;
+                    }
+                }
+
+                if (numero % 2 == 0)
+                {
+                    cantidadPares++;
+                    if (encontrPrimerNumeroPar == false)
+                    {
+                        lugarDelPrimerPar = contador;
+                        encontrPrimerNumeroPar = true;
+                        primerNumeroPar = numero;
+                    }
+                    
+                }
+
+                acumulador += numero;
+            }
+
+            promedio = acumulador / 7;
+            
+            //3,4,5,10,8,2,1
+            
+
+            Console.WriteLine("El numero mas grande es: " + maximo);
+            Console.WriteLine("El numero mas chico es: " + minimo);
+        }
+    }
+}
---
+++ claseUno/primerEj/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primerEj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroUno;
+            int numeroDos;
+            int resultado;
+            //string dato;
+
+            //numeroUno = 3;
+            //numeroDos = 7;
+            Console.WriteLine("Ingrese el primer numero:");
+            while(!int.TryParse(Console.ReadLine(), out numeroUno))
+            {
+                Console.WriteLine("Error, reingrese numero");
+            }
+            Console.WriteLine("Ingrese el segundo numero:");
+            while(!int.TryParse(Console.ReadLine(), out numeroDos))
+            {
+                Console.WriteLine("Error, reingrese numero");
+            }
+            
+            
+            resultado = numeroUno + numeroDos;
+
+            Console.Write("El resultado es "+ resultado); 
+            Console.Read();
+        }
+    }
+}
